100899d5738890e1d783f4b1138dff5f14dba463
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-05-21 22:02:04 -0700

    completed check

--- src/NbQuickCheck.java
+++ src/NbQuickCheck.java
@@ -1,7 +1,8 @@
 import java.util.List;
 import java.util.Map;
 
-public class NbQuickCheck {
+public class NbQuickCheck +
+{
 
   /**
    * Performs a pre-order traversal of the tree, printing each node on a separate line.
@@ -10,10 +11,24 @@ public class NbQuickCheck {
    * @param tree the tree represented as a map of parent nodes to child lists
    * @param root the root node to start traversal from
    */
-  public static void preOrder(Map<Integer, List<Integer>> tree, int root) {
-    if(!tree.containsKey(root)) {
+  public static void preOrder(Map<Integer, List<Integer>> tree, int root) 
+  {
+    //base case, tree is null or tree (map) so contains no root
+    if (tree == null || !tree.containsKey(root))
+    {
+      //return 
       return;
     }
+    //preorder so traverse but visit before the children
+   System.out.println(root);
+
+   //get the children of the root
+   //move in the data map with variable node enter key tree and value root
+   for(Integer node : tree.get(root))
+   {
+    //recurse. visit the children and below
+    preOrder(tree, node);
+   }
   }
 
   /**
@@ -23,8 +38,36 @@ public class NbQuickCheck {
    * @param root the root node of the tree
    * @return the minimum value in the tree or Integer.MAX_VALUE if root is null
    */
-  public static int minVal(Node<Integer> root) {
-    return -1;
+  public static int minVal(Node<Integer> root) 
+  {
+    //if root is null return Integer.MAX_VALUE 
+    if (root == null)
+    {
+      return Integer.MAX_VALUE;
+    }
+
+    //set root value to min
+    //this part held me up a bit
+    //this was something ive knwoen just forgot 
+    int min = root.value;
+
+    for (Node<Integer> node : root.children)
+    {
+      //min in the child
+      //this part made me overthink a bit as well
+      //it makes sense after
+      int minChild = minVal(node); 
+
+      //if minchild is less than root / min 
+      if(minChild < min)
+      {
+        //update
+        min = minChild;
+      }
+    }
+
+
+    return min;
   }
   
 }
